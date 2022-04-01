55e81c1f03f9210121eaa4d7ad0ed408ba31b8b4
BerinGudic <berin.gudic@gmail.com>
2022-04-01 12:36:50 +0200

    Added index.js file

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    var myFirstName = "Berin";
+    var myLastName = "Gudic";
+    var myHomeTown = "Bugojno";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log (main());
+module.exports = main;
